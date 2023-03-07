02e0b84cf2e83c82ea3dd69db9872fd5b66c2bb1
Thong Phan <hewhoipia@gmail.com>
2023-03-07 17:48:21 +0700

    6th

--- knight.h
+++ knight.h
@@ -8,12 +8,16 @@ void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, in
 
 class adventure{
     private:
+        // general
         string file_input;
         int maxHP, HP, level, remedy, maidenkiss, phoenixdown, rescue=-1;
         string events;
         string packages;
         int eSize=0;
         int* eventsArr=nullptr;
+
+        // events
+        size_t tiny=0;
     public:
         adventure(string &_file_input, int& _HP, int& _level, int& _remedy, int& _maidenkiss, int& _phoenixdown, int& _rescue){
             this->file_input=_file_input;
@@ -63,16 +67,18 @@ class adventure{
         // action
         void actionEvents(int eventsName, int num){
             if (eventsName == -1){
+                tinyCheck();
                 return;
             }
             //events
 
             if (eventsName == 0){
                 rescue=1;
-                displayEach(eventsName);
+                displayEach(eventsName, num);
                 return;
             }
             else if (eventsName == 1){
+                tinyCheck();
                 int b = num+1 % 10;
                 int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
                 if (level > lvo){
@@ -82,13 +88,14 @@ class adventure{
                 else if (level < lvo){
                     HP-=(1*lvo*10);
                     if (!HPCheck()){
-                        displayEach(eventsName);
+                        displayEach(eventsName, num);
                         return;
                     }
                 }
-                displayEach(eventsName);
+                displayEach(eventsName, num);
             }
             else if (eventsName == 2){
+                tinyCheck();
                 int b = num+1 % 10;
                 int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
                 if (level > lvo){
@@ -98,13 +105,14 @@ class adventure{
                 else if (level < lvo){
                     HP-=(1.5*lvo*10);
                     if (!HPCheck()){
-                        displayEach(eventsName);
+                        displayEach(eventsName, num);
                         return;
                     }
                 }
-                displayEach(eventsName);
+                displayEach(eventsName, num);
             }
             else if (eventsName == 3){
+                tinyCheck();
                 int b = num+1 % 10;
                 int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
                 if (level > lvo){
@@ -114,13 +122,14 @@ class adventure{
                 else if (level < lvo){
                     HP-=(4.5*lvo*10);
                     if (!HPCheck()){
-                        displayEach(eventsName);
+                        displayEach(eventsName, num);
                         return;
                     }
                 }
-                displayEach(eventsName);
+                displayEach(eventsName, num);
             }
             else if (eventsName == 4){
+                tinyCheck();
                 int b = num+1 % 10;
                 int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
                 if (level > lvo){
@@ -130,13 +139,14 @@ class adventure{
                 else if (level < lvo){
                     HP-=(7.5*lvo*10);
                     if (!HPCheck()){
-                        displayEach(eventsName);
+                        displayEach(eventsName, num);
                         return;
                     }
                 }
-                displayEach(eventsName);
+                displayEach(eventsName, num);
             }
             else if (eventsName == 5){
+                tinyCheck();
                 int b = num+1 % 10;
                 int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
                 if (level > lvo){
@@ -146,44 +156,69 @@ class adventure{
                 else if (level < lvo){
                     HP-=(9.5*lvo*10);
                     if (!HPCheck()){
-                        displayEach(eventsName);
+                        displayEach(eventsName, num);
                         return;
                     }
                 }
-                displayEach(eventsName);
+                displayEach(eventsName, num);
             }
             else if (eventsName == 6){
-                displayEach(eventsName);
+                if(!tinyCheck()){
+                    int b = num+1 % 10;
+                    int lvo = num+1 > 6 ? (b > 5 ? b : 5) : b;
+                    if (level > lvo){
+                        level+2;
+                        levelCheck();
+                    }
+                    else if (level < lvo){
+                        tiny=4;
+                    }
+                    displayEach(eventsName, num);
+                }
+                else{
+                    displayEach(eventsName, num);
+                }
+                
             }
             else if (eventsName == 7){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 11){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 12){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 13){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 15){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 16){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 17){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 18){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 19){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
             else if (eventsName == 99){
-                displayEach(eventsName);
+                // tiny check
+                displayEach(eventsName, num);
             }
 
             return actionEvents(keepTrackE(num+1),num+1);
@@ -201,13 +236,18 @@ class adventure{
             if (HP <= 0){
                 if (phoenixdown >=1){
                     HP = maxHP;
+                    tiny=0;
                     phoenixdown--;
                     return 1;
                 }
                 else {
-                    rescue = -1;
+                    rescue = 0;
                     return 0;
                 }
+            }
+            else if (HP > maxHP){
+                HP=maxHP;
+                return 1;
             }
             else return 1;
         }
@@ -215,6 +255,28 @@ class adventure{
             if (level >= 10){
                 level=10;
             }
+        }
+        bool tinyCheck(){
+            if (tiny>1){
+                if (remedy>=1){
+                    remedy--;
+                    tiny=0;
+                    return 0;
+                }
+                if (tiny==4){
+                    if(HP<5)HP=1;
+                    else HP/=5;
+                }
+                tiny--;
+                return 1;
+            }
+            else if (tiny==1) {
+                tiny=0;
+                HP*=5;
+                HPCheck();
+                return 0;
+            }
+            return 0;
         }
 
         // display
@@ -227,8 +289,8 @@ class adventure{
             cout << endl;
             cout << packages << endl;
         }
-        void displayEach(int eventsName){
-            cout << "eventsName: " << eventsName << endl;
+        void displayEach(int eventsName, int num){
+            cout << "~" << num << " eventsName: " << eventsName << endl;
             cout << HP << " " << level << " " << remedy << " " << maidenkiss << " " << phoenixdown << endl;
         }
 };
