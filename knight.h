47881f52ea132964f53a9d5ce46cec3521a560d4
Thong Phan <hewhoipia@gmail.com>
2023-03-07 13:22:02 +0700

    2nd

--- knight.h
+++ knight.h
@@ -5,5 +5,5 @@
 
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
-
+void readFile(const string& filename, int& HP, int& level, int& remedy, int& maidenkiss, int& phoenixdown, int*& events, string& packages);
 #endif // __KNIGHT_H__
