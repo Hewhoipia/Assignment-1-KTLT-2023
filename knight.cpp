e80d368adcae3fc17ee97113c746fe7d6cb45553
Thong Phan <hewhoipia@gmail.com>
2023-03-07 15:32:16 +0700

    4th

--- knight.cpp
+++ knight.cpp
@@ -10,50 +10,6 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
 }
 
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue) {
-    string events;
-    string packages;
-    readFile(file_input, HP, level, remedy, maidenkiss, phoenixdown, events, packages);
-    int eSize=0;
-    int* eventsArr = modifyEvents(events, eSize);
-
-
-    //display
-    cout << HP << " " << level << " " << remedy << " " << maidenkiss << " " << phoenixdown << endl;
-    cout << events << endl << packages << endl;
-    for (int i=0; i<eSize; i++){
-        cout << eventsArr[i] << " ";
-    }
+    adventure thong (file_input, HP, level, remedy, maidenkiss, phoenixdown, rescue);
     //display (HP, level, remedy, maidenkiss, phoenixdown, rescue);
-}
-
-void readFile(const string& filename, int& HP, int& level, int& remedy, int& maidenkiss, int& phoenixdown, string& events, string& packages) {
-    ifstream myfile(filename);
-    if (myfile.is_open()) {
-        myfile >> HP >> level >> remedy >> maidenkiss >> phoenixdown;
-        getline (myfile, events);
-        getline (myfile, events);
-        getline(myfile, packages);
-        getline(myfile, packages);
-        myfile.close();
-        return;
-    }
-    else {
-        cout << "Cannot read input file";
-        return;
-    }
-}
-
-int* modifyEvents(string& events, int& eSize){
-    int* arr = new int ();
-    stringstream s (events);
-    while (s >> arr[eSize]){
-        eSize++;
-    }
-    return &(*arr);
-}
-
-bool modifyPac(string& packages);
-
-void actionEvents(int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue, string& eventsNum){
-    
 }
--- knight.h
+++ knight.h
@@ -5,6 +5,75 @@
 
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
-void readFile(const string& filename, int& HP, int& level, int& remedy, int& maidenkiss, int& phoenixdown, string& events, string& packages);
-int* modifyEvents(string& events, int& eSize);
+
+class adventure{
+    private:
+        string file_input;
+        int HP, level, remedy, maidenkiss, phoenixdown, rescue;
+        string events;
+        string packages;
+        int eSize=0;
+        int* eventsArr=nullptr;
+    public:
+        adventure(string &_file_input, int& _HP, int& _level, int& _remedy, int& _maidenkiss, int& _phoenixdown, int& _rescue){
+            this->file_input=_file_input;
+            readFile();
+            eventsArr = modifyEvents();
+            actionEvents(keepTrackE(0));
+            
+            //pass value to origin
+            _HP = HP;
+            _level = level;
+            _remedy = remedy;
+            _maidenkiss = maidenkiss;
+            _phoenixdown = phoenixdown;
+            _rescue = rescue;
+            display();
+        }
+    public:
+        void readFile(){
+            ifstream myfile(file_input);
+            if (myfile.is_open()) {
+                myfile >> HP >> level >> remedy >> maidenkiss >> phoenixdown;
+                getline (myfile, events);
+                getline (myfile, events);
+                getline(myfile, packages);
+                getline(myfile, packages);
+                myfile.close();
+                return;
+            }
+            else {
+                cout << "Cannot read input file";
+                return;
+            }
+        }
+        int* modifyEvents(){
+            int* arr = new int ();
+            stringstream s (events);
+            while (s >> arr[eSize]){
+                eSize++;
+            }
+            return &(*arr);
+        }
+        void actionEvents(int eventsName){
+            if (eventsName == -1){
+                return;
+            }
+        }
+        int keepTrackE(int num){
+            if (num == eSize){
+                return -1;
+            }
+            else return eventsArr[num];
+        }
+        bool modifyPac(string& packages);
+        void display(){
+            cout << HP << " " << level << " " << remedy << " " << maidenkiss << " " << phoenixdown << endl;
+            cout << packages << endl;
+            for (int i=0; i<eSize; i++){
+                cout << eventsArr[i] << " ";
+            }
+            cout << endl;
+        }
+};
 #endif // __KNIGHT_H__
